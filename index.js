bd8b877a921a63f64888c6fb8830e37f9ed23922
Breanne <BreMarotta@gmail.com>
2021-11-23 12:49:09 -0500

    commit tree 2b7c9cfa1c5a98d01a62d23d3a27dea3642c9f2c

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Add your code here
+function submitData(userName = "Bob", userEmail = "Bobo@fred.com"){
+    const formData = {
+        name: `${userName}`,
+        email: `${userEmail}`, 
+    };
+    return fetch('http://localhost:3000/users',{
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        },
+        body: JSON.stringify(formData), 
+    })
+    .then(res => res.json())
+    .then(data => {
+        let p = document.createElement('p')
+        p.textContent = data.id
+        document.querySelector('body').appendChild(p)
+    })
+    .catch(function (error) {
+        alert("NOPE, not today!");
+        let p = document.createElement('p')
+        p.textContent = error.messge = ("Unauthorized Access")
+        document.querySelector('body').appendChild(p);
+    });
+ 
+}
+submitData();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
